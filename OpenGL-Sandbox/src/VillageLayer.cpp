02d36a90fd04d4d293a2b39d8538ca4f76ba442d
IvessJohn <vanek2v@gmail.com>
2022-05-04 08:01:11 +0300

    Draw a test rectangle

--- OpenGL-Sandbox/src/VillageLayer.cpp
+++ OpenGL-Sandbox/src/VillageLayer.cpp
@@ -4,7 +4,9 @@ using namespace GLCore;
 using namespace GLCore::Utils;
 
 VillageLayer::VillageLayer()
+	: m_CameraController(16.0f / 9.0f)
 {
+	m_CameraController.GetCamera().SetProjection(0.0f, 1280.0f, 0.0f, 720.0f);
 }
 
 VillageLayer::~VillageLayer()
@@ -15,12 +17,46 @@ void VillageLayer::OnAttach()
 {
 	EnableGLDebugging();
 
-	// Init here
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	m_Shader = Shader::FromGLSLTextFiles(
+		"assets/shaders/test.vert.glsl",
+		"assets/shaders/test.frag.glsl"
+	);
+
+	glUseProgram(m_Shader->GetRendererID());
+
+	glCreateVertexArrays(1, &m_QuadVA);
+	glBindVertexArray(m_QuadVA);
+
+	float vertices[] = {
+		200.0f, 200.0f, 0.0f,
+		300.0f, 300.0f, 0.0f,
+		400.0f, 200.0f, 0.0f,
+	};
+
+	glCreateBuffers(1, &m_QuadVB);
+	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+	uint32_t indices[] = {
+		0, 1, 2
+	};
+	glCreateBuffers(1, &m_QuadIB);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void VillageLayer::OnDetach()
 {
-	// Shutdown here
+	glDeleteVertexArrays(1, &m_QuadVA);
+	glDeleteBuffers(1, &m_QuadVB);
+	glDeleteBuffers(1, &m_QuadIB);
 }
 
 void VillageLayer::OnEvent(Event& event)
@@ -28,12 +64,35 @@ void VillageLayer::OnEvent(Event& event)
 	// Events here
 }
 
+static void SetUniformMat4(uint32_t shader, const char* name, const glm::mat4& matrix)
+{
+	int location = glGetUniformLocation(shader, name);
+	glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+}
+
+static void SetUniformVec4(uint32_t shader, const char* name, const glm::vec4& vec)
+{
+	int location = glGetUniformLocation(shader, name);
+	glUniform4f(location, vec.x, vec.y, vec.z, vec.w);
+}
+
 void VillageLayer::OnUpdate(Timestep ts)
 {
-	// Render here
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(m_Shader->GetRendererID());
+
+	SetUniformMat4(m_Shader->GetRendererID(), "u_ViewProjection", m_CameraController.GetCamera().GetViewProjectionMatrix());
+	SetUniformVec4(m_Shader->GetRendererID(), "u_Color", glm::vec4(0.8f, 0.2f, 0.3f, 1.0f));
+
+	glBindVertexArray(m_QuadVA);
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 }
 
 void VillageLayer::OnImGuiRender()
 {
-	// ImGui here
+	ImGui::Begin("Controls");
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::End();
 }
--- OpenGL-Sandbox/src/VillageLayer.h
+++ OpenGL-Sandbox/src/VillageLayer.h
@@ -15,4 +15,8 @@ public:
 	virtual void OnUpdate(GLCore::Timestep ts) override;
 	virtual void OnImGuiRender() override;
 private:
+	GLCore::Utils::Shader* m_Shader;
+	GLCore::Utils::OrthographicCameraController m_CameraController;
+
+	GLuint m_QuadVA = 0, m_QuadVB = 0, m_QuadIB = 0;
 };
