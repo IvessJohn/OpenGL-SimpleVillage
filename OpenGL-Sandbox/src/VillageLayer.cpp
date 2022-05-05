de45275cf2822267c17224d9c11631265824c056
IvessJohn <vanek2v@gmail.com>
2022-05-05 07:23:57 +0300

    Draw some objects and add methods for batch rendering

--- OpenGL-Sandbox/src/VillageLayer.cpp
+++ OpenGL-Sandbox/src/VillageLayer.cpp
@@ -1,12 +1,32 @@
 #include "VillageLayer.h"
 
+#include <vector>
+
 using namespace GLCore;
 using namespace GLCore::Utils;
+
+struct Vec2 { float x, y; Vec2(float x, float y) : x(x), y(y) {} };
+struct Vec3 { float x, y, z; Vec3(float x, float y, float z) : x(x), y(y), z(z) {} };
+struct Vec4 { float x, y, z, w; Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {} };
+
+struct Vertex
+{
+	Vec3 Position;
+	Vec4 Color;
+	Vec2 TexCoords;
+	float TexID;
+	Vertex(
+		Vec3 pos = { 0.0f, 0.0f, 0.0f }, 
+		Vec4 color = { 0.0f, 0.0f, 0.0f, 0.0f }, 
+		Vec2 texCoords = { 0.0f, 0.0f }, 
+		float texID = 0.0f)
+		: Position(pos), Color(color), TexCoords(texCoords), TexID(texID) {}
+};
 
 VillageLayer::VillageLayer()
 	: m_CameraController(16.0f / 9.0f)
 {
-	m_CameraController.GetCamera().SetProjection(0.0f, 1280.0f, 0.0f, 720.0f);
+	m_CameraController.GetCamera().SetProjection(0.0f, 1280.0f, 720.0f, 0.0f);
 }
 
 VillageLayer::~VillageLayer()
@@ -28,25 +48,70 @@ void VillageLayer::OnAttach()
 
 	glUseProgram(m_Shader->GetRendererID());
 
+	const size_t MaxQuadCount = 16;
+	const size_t MaxVertexCount = MaxQuadCount * 4;
+	const size_t MaxIndexCount = MaxQuadCount * 6;
+
 	glCreateVertexArrays(1, &m_QuadVA);
 	glBindVertexArray(m_QuadVA);
 
+	// Vertices.
+	// Keep in mind that the window is 1280x720
 	float vertices[] = {
-		200.0f, 200.0f, 0.0f,
-		300.0f, 300.0f, 0.0f,
-		400.0f, 200.0f, 0.0f,
+		// Sun
+
+		// Cloud - Small
+		375.0f, 120.0f, 0.0f,
+		540.0f, 120.0f, 0.0f,
+		540.0f, 75.0f, 0.0f,
+		375.0f, 75.0f, 0.0f,
+
+		// Cloud - Big
+		505.0f, 100.0f, 0.0f,
+		750.0f, 100.0f, 0.0f,
+		750.0f, 20.0f, 0.0f,
+		505.0f, 20.0f, 0.0f,
+
+		// Birds
+
+		// Far ground
+		0.0f,	720.0f,	0.0f,
+		1280.0f,720.0f,	0.0f,
+		1280.0f,312.0f,	0.0f,
+		0.0f,	312.0f,	0.0f,
+
+		// Ground
+		0.0f,	720.0f,	0.0f,
+		1280.0f,720.0f,	0.0f,
+		1280.0f,480.0f,	0.0f,
+		0.0f,	480.0f,	0.0f,
+
+		// The house
+
+		// Tree1
 	};
 
 	glCreateBuffers(1, &m_QuadVB);
 	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, MaxVertexCount * sizeof(Vertex), vertices, GL_STATIC_DRAW);
 
 	glEnableVertexArrayAttrib(m_QuadVB, 0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 
-	uint32_t indices[] = {
-		0, 1, 2
-	};
+	uint32_t indices[MaxIndexCount];
+	uint32_t offset = 0;
+	for (size_t i = 0; i < MaxIndexCount; i += 6)
+	{
+		indices[i + 0] = 0 + offset;
+		indices[i + 1] = 1 + offset;
+		indices[i + 2] = 2 + offset;
+
+		indices[i + 3] = 2 + offset;
+		indices[i + 4] = 3 + offset;
+		indices[i + 5] = 0 + offset;
+
+		offset += 4;
+	}
 	glCreateBuffers(1, &m_QuadIB);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -76,9 +141,55 @@ static void SetUniformVec4(uint32_t shader, const char* name, const glm::vec4& v
 	glUniform4f(location, vec.x, vec.y, vec.z, vec.w);
 }
 
+static Vertex* CreateQuad(
+	Vertex* target,
+	const Vec2& pos,
+	const float textureID,
+	const Vec2& size = { 1.0f, 1.0f },
+	const Vec4& color1 = { 0.8f, 0.2f, 0.3f, 1.0f },
+	const Vec4& color2 = { 0.8f, 0.2f, 0.3f, 1.0f },
+	const Vec4& color3 = { 0.8f, 0.2f, 0.3f, 1.0f },
+	const Vec4& color4 = { 0.8f, 0.2f, 0.3f, 1.0f }
+)
+{
+	target->Position = { pos.x, pos.y, 0.0f };
+	target->Color = color1;
+	target->TexCoords = { 0.0f, 0.0f };
+	target->TexID = textureID;
+	target++;
+
+	target->Position = { pos.x + size.x, pos.y, 0.0f };
+	target->Color = color2;
+	target->TexCoords = { 1.0f, 0.0f };
+	target->TexID = textureID;
+	target++;
+
+	target->Position = { pos.x + size.x, pos.y + size.y, 0.0f };
+	target->Color = color3;
+	target->TexCoords = { 1.0f, 1.0f };
+	target->TexID = textureID;
+	target++;
+
+	target->Position = { pos.x, pos.y + size.y, 0.0f };
+	target->Color = color4;
+	target->TexCoords = { 0.0f, 1.0f };
+	target->TexID = textureID;
+	target++;
+
+	return target;
+}
+
 void VillageLayer::OnUpdate(Timestep ts)
 {
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	m_CameraController.OnUpdate(ts);
+
+	uint32_t indexCount = 0;
+
+	std::array<Vertex, 64> vertices;
+	Vertex* buffer = vertices.data();
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f); // Blue BG <- MAKE IT BLUE
+	//glClearColor(0.1f, 0.1f, 0.1f, 1.0f); // Grey BG
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(m_Shader->GetRendererID());
@@ -87,7 +198,7 @@ void VillageLayer::OnUpdate(Timestep ts)
 	SetUniformVec4(m_Shader->GetRendererID(), "u_Color", glm::vec4(0.8f, 0.2f, 0.3f, 1.0f));
 
 	glBindVertexArray(m_QuadVA);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, 128, GL_UNSIGNED_INT, nullptr);
 }
 
 void VillageLayer::OnImGuiRender()
